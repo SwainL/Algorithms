a20cc0ff7b3ea6e3bff61a3a223061aa841eb87e
Swain Liu <liusw0719@gmail.com>
2017-05-21 15:25:54 +0800

    leetcode weekend

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -11,20 +11,19 @@ import java.util.*;
  * Created by liusiwei on 2017/4/19.
  */
 public class Solution {
-    public String toHex(int num) {
-        StringBuilder sb = new StringBuilder();
-        while(num > 1) {
-            sb.append(num % 16);
-            num = num / 16;
-        }
-        return sb.reverse().toString();
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
     }
 
-
-
+    public int getLength(ListNode head) {
+        if(head == null) return 0;
+        return 1 + getLength(head.next);
+    }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.toHex(26));
+        ListNode headA = ListNode.init(new Integer[]{1,2,3,4,5,6});
+
+        System.out.println(sol.getLength(headA));
     }
 }

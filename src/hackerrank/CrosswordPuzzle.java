c6bec09d1d912e3f78e3071473785f707119af67
Swain Liu <liusw0719@gmail.com>
2017-06-25 11:20:30 +0800

    crossword puzzle

---
+++ src/hackerrank/CrosswordPuzzle.java
@@ -0,0 +1,11 @@
+package hackerrank;
+
+/**
+ * Created by lsw on 25/06/2017.
+ */
+public class CrosswordPuzzle {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    }
+}

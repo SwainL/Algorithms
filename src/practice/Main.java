325536c3df1e0f402af0cfbc2fdf990c94236a9b
Swain.L <liusw0719@gmail.com>
2017-11-13 16:32:21 +0800

    Xor

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -8,76 +8,56 @@ import java.util.Scanner;
  * Created by lsw on 14/09/2017.
  */
 public class Main {
-    static int[] nums = null;
-    static int[] tree = null;
-    static int[] indicators = null;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        nums = new int[N];
-        for(int i = 0; i < N; i++) {
-            nums[i] = sc.nextInt();
-        }
-        indicators = new int[N];
-        for(int i = 0; i < N; i++) {
-            indicators[i] = 1;
-        }
-        tree = new int[N * 2];
-        int Q = sc.nextInt();
-        int[] ans =  new int[Q];
-        int[][] querys = new int[Q][4];
-        for(int i = 0; i < Q; i++) {
-            querys[i][0] = i;
-            querys[i][1] = sc.nextInt();
-            querys[i][2] = sc.nextInt();
-            querys[i][3] = sc.nextInt();
-        }
-        Arrays.sort(querys, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[3] - o2[3];
+        System.out.println(int2Str((int) Math.pow(10, 5)));
+    }
+
+    public static int walk(TrieNode root, String[] words, char[] bases) {
+        int count = 0;
+        for (String word : words) {
+            char[] digits = word.toCharArray();
+            for(int i = 0; i < bases.length; i++) {
+                if (bases[i] == '0') {
+                    if (digits[i] == '0') {
+                        count +=
+                    }
+                } else if (bases[i] == '1') {
+                    if (digits[i] == '0') {
+                        count +=
+                    }
+                }
             }
-        });
-        System.out.println(Arrays.deepToString(querys));
-        build(1, 0, N - 1);
-        System.out.println(Arrays.toString(tree));
-        for(int i = 0; i < Q; i++) {
-            for(int j = 0; j < N; j++) {
-                if(nums[j] <= querys[i][3]) update(j, 1, 0, N - 1);
+        }
+
+
+    }
+
+    public static TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            char[] digits = word.toCharArray();
+            TrieNode cur = root;
+            for (int i = 0; i < digits.length; i++) {
+                if(cur.links[digits[i] - '0'] == null) {
+                    cur.links[digits[i] - '0'] = new TrieNode();
+                }
+                cur = cur.links[digits[i] - '0'];
+                cur.paths++;
             }
-            ans[querys[i][0]] = query(1, 0, N - 1, querys[i][1] - 1, querys[i][2] -1);
         }
-
-        System.out.println(Arrays.toString(ans));
+        return root;
     }
 
-    public static void build(int node, int start, int end) {
-        if(start == end) {
-            tree[node] = indicators[start];
-        } else {
-            int mid = start + (end - start) / 2;
-            build(2 * node, start, mid);
-            build(2 * node + 1, mid + 1, end);
-            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    public static String int2Str(int num) {
+        // 10 ^ 5 = 11000011010100000 (17 bits in total)
+        // to guarantee the precision, we need at 17 bits to represent a binary number
+        char[] digits = new char[17];
+        for(int i = 0; i < 17; i++) {
+            digits[16 - i] = (num >> i & 1) == 0 ? '0' : '1';
         }
+        return String.valueOf(digits);
     }
 
-    public static void update(int pos, int node, int start, int end) {
-        if(start == end) {
-            tree[node] = 0;
-            return;
-        }
-        int mid = start + (end - start) / 2;
-        if(pos <= mid) update(pos, 2 * node, start, mid);
-        else update(pos, 2 * node + 1, mid + 1, end);
-        tree[node] = tree[node * 2] + tree[node * 2 + 1];
-    }
 
-    public static int query(int node, int start, int end, int l, int r) {
-        if(r < start || l > end) return 0;
-        if(l <= start && r >= end) return tree[node];
-        int mid = start + (end - start) / 2;
-        return query(node * 2, start, mid, l, r) + query(node * 2 + 1, mid + 1, end, l, r);
-    }
 }
